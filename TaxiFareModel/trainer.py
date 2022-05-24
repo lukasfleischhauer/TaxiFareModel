1f71acd1afd1bbbfe1c75f0f91df97015333ce6d
Lukas Fleischhauer <lukas.fleischhauer@gmail.com>
2022-05-24 21:13:28 +0100

    Initial Push

---
+++ TaxiFareModel/trainer.py
@@ -0,0 +1,111 @@
+# imports
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.pipeline import Pipeline
+from TaxiFareModel.encoders import DistanceTransformer, TimeFeaturesEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import LinearRegression
+from TaxiFareModel.utils import compute_rmse
+from TaxiFareModel.data import get_data, clean_data
+from sklearn.model_selection import train_test_split
+from memoized_property import memoized_property
+import mlflow
+from  mlflow.tracking import MlflowClient
+import joblib
+
+
+
+
+
+
+class Trainer():
+    MLFLOW_URI = "https://mlflow.lewagon.ai/"
+    experiment_name = "[PT] [Lisbon] [lukasfleischhauer] taxifaremodel + 1.03"
+
+    def __init__(self, X, y):
+        """
+            X: pandas DataFrame
+            y: pandas Series
+        """
+        self.pipeline = None
+        self.X = X
+        self.y = y
+
+    def set_pipeline(self):
+        """defines the pipeline as a class attribute"""
+        dist_pipe = Pipeline([
+            ('dist_trans', DistanceTransformer()),
+            ('stdscaler', StandardScaler())
+        ])
+        time_pipe = Pipeline([
+            ('time_enc', TimeFeaturesEncoder('pickup_datetime')),
+            ('ohe', OneHotEncoder(handle_unknown='ignore'))
+        ])
+        preproc_pipe = ColumnTransformer([
+            ('distance', dist_pipe, ["pickup_latitude", "pickup_longitude", 'dropoff_latitude', 'dropoff_longitude']),
+            ('time', time_pipe, ['pickup_datetime'])
+        ], remainder="drop")
+        pipe = Pipeline([
+            ('preproc', preproc_pipe),
+            ('linear_model', LinearRegression())
+        ])
+        self.mlflow_log_param('model', 'Linear')
+        return pipe
+
+    def run(self):
+        """set and train the pipeline"""
+        self.pipe = self.set_pipeline()
+        self.pipe.fit(self.X, self.y)
+
+    def evaluate(self, X_test, y_test):
+        """evaluates the pipeline on df_test and return the RMSE"""
+        y_pred = self.pipe.predict(X_test)
+        rmse = compute_rmse(y_pred, y_test)
+        print(rmse)
+        self.mlflow_log_metric('rmse', rmse)
+        return rmse
+
+    @memoized_property
+    def mlflow_client(self):
+        mlflow.set_tracking_uri(self.MLFLOW_URI)
+        return MlflowClient()
+
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.experiment_name)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(self.experiment_name).experiment_id
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
+
+    def save_model(self):
+        """ Save the trained model into a model.joblib file """
+        joblib.dump(self.pipe, 'taxifaremodel.joblib')
+
+
+if __name__ == "__main__":
+    # get data
+    df = get_data()
+    # clean data
+    df = clean_data(df)
+    # set X and y
+    y = df["fare_amount"]
+    X = df.drop("fare_amount", axis=1)
+    # hold out
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.15)
+    # train
+    tr = Trainer(X_train, y_train)
+    tr.run()
+    # evaluate
+    rmse = tr.evaluate(X_val, y_val)
+    print('rmse')
+    # save
+    tr.save_model()
